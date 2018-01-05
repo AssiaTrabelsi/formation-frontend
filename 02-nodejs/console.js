fb53c24e252368551c1e5768fe5a603247842828
Asia Trabelsi <assia.trabelsi93@gmail.com>
2018-01-05 09:03:06 +0100

    02-node-tp-fini

--- 02-nodejs/console.js
+++ 02-nodejs/console.js
@@ -1,51 +1,55 @@
-var dataService = require('./service')
-var readline = require('readline')
+var dateService = require("./service");
 
+const readline = require('readline'),
+    rl = readline.createInterface(process.stdin, process.stdout);
 
-function AppConsole(dataService) {
-    const menu =
-        [
-            { libelle: 'liste de tous les presentateurs', action: listerPresentateurs },
-            { libelle: 'Top présentateurs', action: topPresentateurs },
-            { libelle: 'liste de sessions', action: listerSessions },
-            { libelle: 'Détail une sessions', action: detailSession }
-
-        ]
-
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-
-    rl.question('What do you think of Node.js? ', (answer) => {
-        menu.forEach(function (m, index) {
-
-            console.log(m)
-            console.log(index)
-        })
-        rl.close();
-    });
+function appConsole() {
+    console.log('*** Application Conférence ***');
+    console.log('1. Liste de tous les présentateurs ');
+    console.log('2. Top présentateurs ');
+    console.log('3. Liste des sessions');
+    console.log('4. Détail dune session');
 }
-
 
 function start() {
+    appConsole();
+    rl.question('What do you think of Node.js? ', function (answer) {
 
+        console.log('Thank you for your valuable feedback:', answer);
+
+
+        if (answer.match('1')) {
+            dateService.listerTousLesPresentateurs().forEach(p => {
+                console.log(p.firstname + ' ' + p.lastname);
+            });
+            rl.close();
+
+        } else if (answer.match('2')) {
+            dateService.listerTopPresentateurs().forEach(t => {
+                console.log(t.firstname + ' ' + t.lastname);
+
+            });
+            rl.close();
+
+        } else if (answer.match('3')) {
+            dateService.listerToutesLesSessions().forEach(s => {
+                console.log(s.id + ' ' + s.title);
+
+            });
+            rl.close();
+
+        } else if (answer.match('4')) {
+            rl.question('Quelle session? ', function (answer) {
+                session = dateService.trouverUneSession(answer);
+                console.log(session);
+                rl.close();
+            })
+
+        } else {
+            console.log('Ended');
+            rl.close();
+        }
+
+    });
 }
-AppConsole(dataService);
-function listerPresentateurs() {
-    dataService.listerTousLesPresentateurs().forEach(p => p.console.log(p.firstname))
-}
-
-function topPresentateurs() {
-    dataService.listerTopPresentateurs().filter(t => t.console.log(t.topspeaker))
-}
-
-function listerSessions() {
-    dataService.listerToutesLesSessions().forEach(s => s.console.log(s.title))
-}
-
-function detailSession() {
-
-}
-
-
+start();
